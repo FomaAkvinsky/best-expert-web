15f5d35307b35a76443849c1770ed0e4ff92d41b
FomaAkvinsky <avavto@gmail.com>
2026-09-23 17:05:45 +0600

    Complete BEST content component foundation

---
+++ local/components/best/service.detail/templates/.default/blocks/cta.php
@@ -0,0 +1,25 @@
+<?php
+$props = $block['PROPERTIES'];
+$label = trim((string)($props['CTA_LABEL']['VALUE'] ?? 'Оставить запрос'));
+$url = trim((string)($props['CTA_URL']['VALUE'] ?? '#b24-form'));
+?>
+<section class="section bg-gray-700 particles-js-outer">
+  <div class="container">
+    <div class="row justify-content-center justify-content-xl-between align-items-center">
+      <div class="col-lg-8">
+        <div class="section-lg">
+          <?php if (!empty($props['EYEBROW']['VALUE'])): ?>
+            <h6><?=htmlspecialcharsbx((string)$props['EYEBROW']['VALUE'])?></h6>
+          <?php endif; ?>
+          <h2><?=htmlspecialcharsbx($block['NAME'])?></h2>
+          <?php if (!empty($props['INTRO']['VALUE'])): ?>
+            <p class="lead"><?=nl2br(htmlspecialcharsbx((string)$props['INTRO']['VALUE']))?></p>
+          <?php endif; ?>
+        </div>
+      </div>
+      <div class="col-lg-3 pb-4">
+        <a class="button button-primary" href="<?=htmlspecialcharsbx($url)?>"><?=htmlspecialcharsbx($label)?></a>
+      </div>
+    </div>
+  </div>
+</section>

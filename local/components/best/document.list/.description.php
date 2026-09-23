b72147d614c97c5235e1e5968a0b64b93a4504c1
FomaAkvinsky <avavto@gmail.com>
2026-09-23 17:04:39 +0600

    Add BEST content platform foundation

---
+++ local/components/best/document.list/.description.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$arComponentDescription = [
+    'NAME' => 'БЭСТ: документы',
+    'DESCRIPTION' => 'Выводит документы, сертификаты и аккредитации БЭСТ.',
+    'PATH' => [
+        'ID' => 'best',
+        'NAME' => 'БЭСТ',
+    ],
+];

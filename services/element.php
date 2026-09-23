0c676f8f4aaecf9cc36baadcb3b1eca45ed0438a
FomaAkvinsky <avavto@gmail.com>
2026-09-23 20:17:40 +0600

    Pass parent service section code to element component

--- services/element.php
+++ services/element.php
@@ -7,6 +7,7 @@ $APPLICATION->IncludeComponent(
     [
         'IBLOCK_CODE' => 'best_services',
         'BLOCKS_IBLOCK_CODE' => 'best_service_blocks',
+        'SECTION_CODE' => (string)($_GET['SECTION_CODE'] ?? ''),
         'CODE' => (string)($_GET['CODE'] ?? ''),
         'CACHE_TYPE' => 'A',
         'CACHE_TIME' => 3600,

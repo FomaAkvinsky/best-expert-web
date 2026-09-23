9242d2cfa1136ed0704c812d8520bbd882588559
FomaAkvinsky <avavto@gmail.com>
2026-09-23 20:04:26 +0600

    Add generic service section and element routing foundation

---
+++ local/components/best/service.section/.description.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$arComponentDescription = [
+    'NAME' => 'БЭСТ: раздел услуг',
+    'DESCRIPTION' => 'Формирует страницу категории услуг по символьному коду раздела инфоблока.',
+    'PATH' => [
+        'ID' => 'best',
+        'NAME' => 'БЭСТ',
+    ],
+];

ebfd5737ffd254a3fc70251d58fbd12978f1437f
FomaAkvinsky <avavto@gmail.com>
2026-09-23 20:04:39 +0600

    Add generic service section and element routing foundation

---
+++ services/element.php
@@ -0,0 +1,17 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+$APPLICATION->IncludeComponent(
+    'best:service.detail',
+    '',
+    [
+        'IBLOCK_CODE' => 'best_services',
+        'BLOCKS_IBLOCK_CODE' => 'best_service_blocks',
+        'CODE' => (string)($_GET['CODE'] ?? ''),
+        'CACHE_TYPE' => 'A',
+        'CACHE_TIME' => 3600,
+    ]
+);
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>

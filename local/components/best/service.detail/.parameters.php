84a280fafa0b50d604a28c1873528425bbb3ef1d
FomaAkvinsky <avavto@gmail.com>
2026-09-23 17:04:51 +0600

    Add BEST content platform foundation

---
+++ local/components/best/service.detail/.parameters.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$arComponentParameters = [
+    'PARAMETERS' => [
+        'IBLOCK_CODE' => [
+            'PARENT' => 'BASE',
+            'NAME' => 'Код инфоблока услуг',
+            'TYPE' => 'STRING',
+            'DEFAULT' => 'best_services',
+        ],
+        'BLOCKS_IBLOCK_CODE' => [
+            'PARENT' => 'BASE',
+            'NAME' => 'Код инфоблока блоков',
+            'TYPE' => 'STRING',
+            'DEFAULT' => 'best_service_blocks',
+        ],
+        'ELEMENT_CODE' => [
+            'PARENT' => 'BASE',
+            'NAME' => 'Символьный код услуги',
+            'TYPE' => 'STRING',
+            'DEFAULT' => '={$_REQUEST["ELEMENT_CODE"]}',
+        ],
+        'CACHE_TIME' => ['DEFAULT' => 3600],
+    ],
+];

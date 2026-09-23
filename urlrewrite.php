098ce7c53583c0e3755f8d11540e4a8a258fa012
FomaAkvinsky <avavto@gmail.com>
2026-09-23 17:05:57 +0600

    Route judicial expertise detail pages through content component

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,6 +1,14 @@
 <?php
 $arUrlRewrite=array (
-  0 => 
+  0 =>
+  array (
+    'CONDITION' => '#^/services/sudebnye-ekspertizy/([^/]+)/?$#',
+    'RULE' => 'ELEMENT_CODE=$1',
+    'ID' => 'best:service.detail',
+    'PATH' => '/services/sudebnye-ekspertizy/detail.php',
+    'SORT' => 50,
+  ),
+  1 =>
   array (
     'CONDITION' => '#^/rest/#',
     'RULE' => '',

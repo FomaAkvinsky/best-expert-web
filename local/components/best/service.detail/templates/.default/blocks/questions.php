bc159795369cda6b16ed3b9732e3ad16875e5a09
FomaAkvinsky <avavto@gmail.com>
2026-09-24 18:37:07 +0600

    Make questions block layout configurable

--- local/components/best/service.detail/templates/.default/blocks/questions.php
+++ local/components/best/service.detail/templates/.default/blocks/questions.php
@@ -2,20 +2,28 @@
 $props = $block['PROPERTIES'];
 $items = (array)($props['ITEMS']['VALUE'] ?? []);
 $eyebrow = trim((string)($props['EYEBROW']['VALUE'] ?? ''));
+
+$sectionClass = bestServiceBlockClass($block, 'SECTION_CLASS', 'section section-lg bg-white');
+$containerClass = bestServiceBlockClass($block, 'CONTAINER_CLASS', 'container');
+$headingRowClass = bestServiceBlockClass($block, 'HEADING_ROW_CLASS', 'row justify-content-center');
+$headingColClass = bestServiceBlockClass($block, 'HEADING_COL_CLASS', 'col-md-10 col-lg-8 text-center');
+$titleClass = bestServiceBlockClass($block, 'TITLE_CLASS', '');
+$contentRowClass = bestServiceBlockClass($block, 'CONTENT_ROW_CLASS', 'row justify-content-center');
+$contentColClass = bestServiceBlockClass($block, 'CONTENT_COL_CLASS', 'col-md-10 col-lg-9');
 ?>
-<section class="section section-lg bg-white">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10 col-lg-8 text-center">
+<section class="<?=htmlspecialcharsbx($sectionClass)?>">
+    <div class="<?=htmlspecialcharsbx($containerClass)?>">
+        <div class="<?=htmlspecialcharsbx($headingRowClass)?>">
+            <div class="<?=htmlspecialcharsbx($headingColClass)?>">
                 <?php if ($eyebrow !== ''): ?><h6><?=htmlspecialcharsbx($eyebrow)?></h6><?php endif; ?>
-                <h2><?=htmlspecialcharsbx($block['NAME'])?></h2>
+                <h2<?php if ($titleClass !== ''): ?> class="<?=htmlspecialcharsbx($titleClass)?>"<?php endif; ?>><?=htmlspecialcharsbx($block['NAME'])?></h2>
                 <?php if (!empty($props['INTRO']['VALUE'])): ?>
                     <p><?=nl2br(htmlspecialcharsbx((string)$props['INTRO']['VALUE']))?></p>
                 <?php endif; ?>
             </div>
         </div>
-        <div class="row justify-content-center">
-            <div class="col-md-10 col-lg-9">
+        <div class="<?=htmlspecialcharsbx($contentRowClass)?>">
+            <div class="<?=htmlspecialcharsbx($contentColClass)?>">
                 <div class="services-divider__card">
                     <ul class="list-marked">
                         <?php foreach ($items as $item): ?>
